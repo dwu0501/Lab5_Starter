6fd0197157d5e450c1d216a1cfe3fb5932a7b48f
dwu0501 <dwu0501@gmail.com>
2026-05-05 01:23:06 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Make sure you make a PR to your own repo's main and not the class' repo!! Otherw
 
 ### Members
 - Daniel Wu
+
+## Explore - pt.2
+1) I wouldn't use a unit test to test the "message" feature since we cannot check for the interactions that encompass this feature, like API calls, frontend and backend communication, and user feedback. A unit test cannot account for all of these things at the same time.
+
+2) I would use a unit test to test the "max message length" feature since it is an easy assertion to make on a message component. This can be done as simply as: `expect(message.length).toBeLessThanOrEqual(80)`.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,4 @@
 // unit.test.js
-
 import {
   isPhoneNumber,
   isEmail,
@@ -9,3 +8,86 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// isPhoneNumber tests
+test('valid number - california number', () => {
+  expect(isPhoneNumber("929 990-8838")).toBe(true);
+});
+
+test('valid number - number all dashes', () => {
+  expect(isPhoneNumber("929-788-0909")).toBe(true);
+});
+
+test('invalid number - no dash', () => {
+  expect(isPhoneNumber("65380933")).toBe(false);
+});
+
+test('invalid number - letter added', () => {
+  expect(isPhoneNumber("708-229-099o")).toBe(false);
+});
+
+// isEmail tests
+test('valid email - extra domains', () => {
+  expect(isEmail("cse110@ucsd.edu")).toBe(true);
+});
+
+test('valid email - capitalized combined letters', () => {
+  expect(isEmail("SWEisAwesome@UCSD.io")).toBe(true);
+});
+
+test('invalid email - longer subdomains', () => {
+  expect(isEmail("cse110@ucsd.educateSWE")).toBe(false);
+});
+
+test('invalid email - extra domains and characters', () => {
+  expect(isEmail("cse110@gmail.ucsd.edu.cse#")).toBe(false);
+});
+
+// isStrongPassword tests
+test('valid strong password - normal password', () => {
+  expect(isStrongPassword("ThisIsStrong_01")).toBe(true);
+});
+
+test('valid strong password - normal password', () => {
+  expect(isStrongPassword("sp26")).toBe(true);
+});
+
+ test('invalid strong password - password over length', () => {
+  expect(isStrongPassword("ThisIsStrong_010")).toBe(false);
+});
+
+ test('invalid strong password - password unpermitted chars', () => {
+  expect(isStrongPassword("ThisIsStrong_#")).toBe(false);
+});
+
+
+// isDate tests
+test('valid date - normal date', () => {
+  expect(isDate("01/01/2000")).toBe(true);
+});
+
+test('valid date - compact date', () => {
+  expect(isDate("1/1/2006")).toBe(true);
+});
+
+ test('invalid date - too long', () => {
+  expect(isDate("01/01/20001")).toBe(false);
+});
+
+ test('invalid date - invalid day and month', () => {
+  expect(isDate("321//2026")).toBe(false);
+});
+
+// isHexColor tests
+test('valid HEX colour - normal with interchange capitalized', () => {
+  expect(isHexColor("aDfFEe")).toBe(true);
+});
+test('valid HEX colour - compact black', () => {
+  expect(isHexColor("000")).toBe(true);
+});
+test('invalid HEX colour - over range', () => {
+  expect(isHexColor("GGG")).toBe(false);
+});
+test('invalid HEX colour - over length', () => {
+  expect(isHexColor("aDfFEe0")).toBe(false);
+});
